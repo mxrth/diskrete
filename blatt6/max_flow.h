c753b2993c18420af23eec649f26164bef231004
bnw <s6beklot@uni-bonn.de>
2014-11-16 21:49:02 +0100

    blatt 6 max flow ungetestet

--- blatt6/max_flow.h
+++ blatt6/max_flow.h
@@ -3,6 +3,6 @@
 
 //expects a directed graph with the costs of all edges being one
 //and calculates the maximum flow in this graph
-int get_max_flow(Graph &g);
+int get_max_flow(Graph &G, Graph::NodeId s, Graph::NodeId tId);
 
 #endif
