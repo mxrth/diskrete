ccf22cbb03880576e911bc3dfe7f45d96578633a
drak3 <drak3@live.de>
2014-11-16 21:05:29 +0100

    written max_flow.written max_flow.hh

---
+++ blatt6/max_flow.h
@@ -0,0 +1,8 @@
+#ifndef MAX_FLOW_H
+#define MAX_FLOW_H
+
+//expects a directed graph with the costs of all edges being one
+//and calculates the maximum flow in this graph
+int get_max_flow(Graph &g);
+
+#endif
