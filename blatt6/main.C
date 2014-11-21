fd5ea021307e2430daa8476b328f053ff19a08f6
bnw <s6beklot@uni-bonn.de>
2014-11-21 16:46:47 +0100

    multiple bugfixes main

--- blatt6/main.C
+++ blatt6/main.C
@@ -4,41 +4,34 @@
 using std::vector;
 
 void to_digraph(Graph& G) {
-	vector<bool> visited(G.num_nodes() * 3, false);
-	Graph::NodeId last_node = G.num_nodes() - 1;
-	for (Graph::NodeId v = 0; v < last_node; v++) {
-		visited[v] = true;
-		for (auto e : G.get_node(v).out_edges()) {
-			auto w = G.get_edge(e).get_head();
-			if (visited[w] || w > last_node) {
-				continue;
-			}
-			visited[w] = true;
-
-			Graph::NodeId top = G.add_node();
-			Graph::NodeId bot = G.add_node();
-			G.change_head(e, top);
-			G.add_edge(top, bot);
-			G.add_edge(w, top);
-			G.add_edge(bot, v);
-			G.add_edge(bot, w);
-		}
+	Graph::EdgeId lastEdgeId = G.num_edges();
+	for (Graph::NodeId eId = 0; eId < lastEdgeId; eId++) {
+		auto e = G.get_edge(eId);
+		auto v = e.get_tail();
+		auto w = e.get_head();
+		Graph::NodeId top = G.add_node();
+		Graph::NodeId bot = G.add_node();
+		G.change_head(eId, top);
+		G.add_edge(top, bot);
+		G.add_edge(w, top);
+		G.add_edge(bot, v);
+		G.add_edge(bot, w);
 	}
 }
 
 void preprocess(Graph& G) {
 	Graph::NodeId last_original_node = G.num_nodes() - 1;
 	to_digraph(G);
-	//range over all nodes of the original graph
-	//for each node w, insert a new node v
-	//add a node e=(v,w)
-	//for each incoming node of w, change the head of that edge to v
-	for (Graph::NodeId w = 0; w <= last_original_node; w++) {
+//range over all nodes of the original graph
+//for each node w, insert a new node v
+//add a node e=(v,w)
+//for each incoming node of w, change the head of that edge to v
+	for (Graph::NodeId w = 2; w <= last_original_node; w++) {
 		auto v = G.add_node();
-		G.add_edge(v, w);
 		for (auto incoming : G.get_node(w).in_edges()) {
 			G.change_head(incoming, v);
 		}
+		G.add_edge(v, w);
 	}
 }
 
@@ -49,9 +42,15 @@ int main(int argc, char* argv[]) {
 	}
 	if (argc == 3)
 		debug = true;
+
+	std::cout << "DEBUG = " << debug << std::endl;
+
 	Graph G = read_graph(argv[1]);
-	if (!debug)
+
+	if (!debug){
 		preprocess(G);
+	}
+
 	std::cout << "Number of disjoint paths: " << get_max_flow(G, 0, 1)
 			<< std::endl;
 }
