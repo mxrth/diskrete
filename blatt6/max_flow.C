39ed09715d3a9f59bab450dd6035b930ef6bf952
bnw <s6beklot@uni-bonn.de>
2014-11-22 21:39:45 +0100

    debugging tools fuer maxflow algo

--- blatt6/max_flow.C
+++ blatt6/max_flow.C
@@ -56,7 +56,7 @@ bool dfs(Graph &G, std::vector<bool> &flow, Graph::NodeId tId,
 
 int get_max_flow(Graph &G, Graph::NodeId s, Graph::NodeId tId) {
 
-	std::vector<bool> flow(G.num_edges(), 0);
+	std::vector<bool> flow(G.num_edges(), false);
 	int anzahlWege = 0;
 
 	while (true) {
@@ -69,3 +69,39 @@ int get_max_flow(Graph &G, Graph::NodeId s, Graph::NodeId tId) {
 		}
 	}
 }
+int countTotalFlow(Graph &G, Graph::NodeId s, std::vector<bool> &flow){
+	int sum = 0;
+	for(Graph::NodeId eId = 0; eId < G.num_edges(); eId++){
+		sum += flow[eId];
+	}
+	return sum;
+}
+
+int countOutFlow(Graph &G, Graph::NodeId s, std::vector<bool> &flow){
+	int sum = 0;
+	for(auto eId : G.get_node(s).out_edges()){
+		sum += flow[eId];
+	}
+	return sum;
+}
+
+int countInFlow(Graph &G, Graph::NodeId s, std::vector<bool> &flow){
+	int sum = 0;
+	for(auto eId : G.get_node(s).in_edges()){
+		sum += flow[eId];
+	}
+	return sum;
+}
+
+int checkIntegrity(Graph &G, std::vector<bool> &flow){
+	if(countInFlow(G,1, flow) != countOutFlow(G, 0, flow)){
+		return 1;
+	}
+	for(Graph::NodeId vId = 2; vId < G.num_nodes(); vId++){
+		if(countInFlow(G, vId, flow) != countOutFlow(G, vId, flow)){
+			return 2;
+		}
+	}
+	return 0;
+}
+
