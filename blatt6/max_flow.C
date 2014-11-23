b207c2b0e4a860aaf16126008117a864e615eee5
drak3 <drak3@live.de>
2014-11-23 22:44:57 +0100

    implementing dfs nonrecursive
    visited is now local to the dfs function

--- blatt6/max_flow.C
+++ blatt6/max_flow.C
@@ -1,6 +1,9 @@
 #include "graph.h"
 #include <set>
+#include <stack>
 
+using std::stack;
+using std::vector;
 
 /*
  * Gibt für eine gegebene Kante eId und einen Endpunkt vId dieser Kante
@@ -14,43 +17,54 @@ Graph::NodeId getOtherNode(Graph &G, Graph::EdgeId eId, Graph::NodeId vId) {
 	return e.get_head();
 }
 
-
-bool dfs(Graph &G, std::vector<bool> &flow, Graph::NodeId tId,
-		std::vector<bool> &visited, Graph::NodeId currentNodeId) {
-	std::vector<Graph::EdgeId> angrenzendeKanten;
-
-	auto currentNode = G.get_node(currentNodeId);
-	//sammle Kanten
-	for (auto eId : currentNode.in_edges()) {
-		if (flow[eId] == true) {
-			angrenzendeKanten.push_back(eId);
-		}
+Graph::EdgeId findUsableEdge(Graph::NodeId node, Graph &G, vector<bool> &flow, vector<bool> &visited) {
+    auto currentNode = G.get_node(node);
+    //sammle Kanten
+    for (auto eId : currentNode.in_edges()) {
+	if (flow[eId] == true) {
+	    Graph::NodeId next_node = G.get_edge(eId).get_head();
+	    if(!visited[next_node]) {
+		return eId;
+	    }
 	}
-	for (auto eId : currentNode.out_edges()) {
-		if (flow[eId] == false) {
-			angrenzendeKanten.push_back(eId);
-		}
+    }
+    for (auto eId : currentNode.out_edges()) {
+	if (flow[eId] == false) {
+	    Graph::NodeId next_node = G.get_edge(eId).get_tail();
+	    if(!visited[next_node]) {
+		return eId;
+	    }
 	}
+    }
+    return G.invalid_edge;
+}
 
+bool dfs(Graph &G, std::vector<bool> &flow, Graph::NodeId tId, Graph::NodeId sId) {
 
-	for (Graph::EdgeId eId : angrenzendeKanten) {
-		auto wId = getOtherNode(G, eId, currentNodeId);
-		if (visited[wId] == true) {
-			continue;
-		}
-		visited[wId] = true;
-		flow[eId] = !flow[eId];
-		if (wId == tId) {
-			return true;
-		}
-		if (dfs(G, flow, tId, visited, wId)) {
-			return true;
-		} else {
-			//ueber diese kante kommen wir nicht zum ziel -> rueckgaengeig machen
-			flow[eId] = !flow[eId];
-		}
+    stack<Graph::EdgeId, vector<Graph::EdgeId>> path;
+    vector<bool> visited(G.num_nodes(), false);
+
+    Graph::NodeId cur = sId;
+    visited[sId] = true;
+
+    while(cur != tId) {
+	Graph::EdgeId e = findUsableEdge(cur, G, flow, visited);
+	if(e != G.invalid_edge) {
+	    flow[e] = !flow[e];
+	    path.push(e);
+	    cur = getOtherNode(G, e, cur);
+	    visited[cur] = true;
+	} else if(e == G.invalid_edge && cur != sId) {
+	    Graph::EdgeId prevEdge = path.top();
+	    path.pop();
+	    flow[prevEdge] = !flow[prevEdge];
+	    Graph::NodeId prevNode = getOtherNode(G, prevEdge, cur);
+	    cur = prevNode;
+	} else {
+	    return false;
 	}
-	return false;
+    }
+    return true;
 }
 
 
@@ -60,9 +74,7 @@ int get_max_flow(Graph &G, Graph::NodeId s, Graph::NodeId tId) {
 	int anzahlWege = 0;
 
 	while (true) {
-		std::vector<bool> visited(G.num_nodes(), false);
-		visited[s] = true;
-		if(dfs(G, flow, tId, visited, s)){
+		if(dfs(G, flow, tId, s)){
 			anzahlWege++;
 		}else{
 			return anzahlWege;
